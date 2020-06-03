396681455e7b7ae9ea2468b7c2840a6f8e617aab
AlexandroMarcelo <alex.fcomarcelo@gmail.com>
2020-06-02 21:09:58 -0500

    Update API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://apistudentexchange.azurewebsites.net/",
   responseType: "json"
 });
 
